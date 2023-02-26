b542d67909cf26ea30cf30c43b9727f679f92490
mattobcodes <muchoperro77@gmail.com>
2023-02-26 16:01:00 +0100

    small style updates

--- script.js
+++ script.js
@@ -49,13 +49,13 @@ function fetchIssues(){
         var assignedTo = issues[i].assignedTo;
         var status = issues[i].status;
 
-        issuesList.innerHTML += '<div class="well">' +
-                                '<h6>Issue ID: ' + id + '</h6>' +
-                                '<p><span class="label label-info">'+ status + '</span></p>' +
+        issuesList.innerHTML += '<div class="well p-3 m-2 bg-body-tertiary rounded-1">' +
+                                '<h6 class="text-muted">Issue ID: ' + id + '</h6>' +
+                                '<p><span class="bg-info p-1 rounded-1 mt-3">'+ status + '</span></p>' +
                                 '<h3>' + desc + '</h3>' +
-                                '<p><span class="glyphicon glyphicon-time"></span>' + severity +'</p>' +
-                                '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo +'</p>' +
-                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
+                                '<p><i class="bi bi-alarm-fill me-2"></i>' + severity +'</p>' +
+                                '<p><i class="bi bi-person-fill me-2"></i>' + assignedTo +'</p>' +
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning m-1">Close</a>' +
                                 '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>' +
                                 '</div>';
     }
